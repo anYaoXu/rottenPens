cec382072fa85186dbd6820dd705e02d8d9c8035
anYaoXu <3131895796@qq.com>
2017-11-02 18:30:30 +0800

    'gruntfile'

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,16 @@
 module.exports = function (grunt) {
-
+    grunt.initConfig({
+        pkg:grunt.file.readJSON("package.json"),
+        uglify:{
+            options:{
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build:{
+                src: 'app/javaScripts/gruntmin.js',
+                dest: 'app/dist/qq.js'
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['uglify']);
 };
---
+++ app/dist/qq.js
@@ -0,0 +1,3 @@
+/*! rottenpens 2017-11-02 */
+
+var dd="123";console.log(dd);
